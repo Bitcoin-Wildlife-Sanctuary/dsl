de42934c0f09bc7e23893ed725d41a9c20d43438
weikengchen <w.k@berkeley.edu>
2024-08-26 03:13:55 +0800

    add test_program

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+use bitcoin_script::script;
+use bitcoin_scriptexec::execute_script;
+use crate::compiler::Compiler;
+use crate::dsl::DSL;
+use crate::treepp::Script;
+
 pub mod data_type;
 
 pub mod functions;
@@ -12,11 +18,32 @@ pub mod stack;
 
 pub mod compiler;
 
-pub mod treepp {
+pub(crate) mod treepp {
     pub use bitcoin_script::{define_pushable, script};
     #[cfg(test)]
     pub use bitcoin_scriptexec::execute_script;
 
     define_pushable!();
     pub use bitcoin::ScriptBuf as Script;
+}
+
+use crate::treepp::*;
+
+pub fn test_program(dsl: DSL) {
+    let program = Compiler::compiler(dsl).unwrap();
+
+    let mut script = script! {
+        for elem in program.hint.iter() {
+            { elem }
+        }
+        for elem in program.input.iter() {
+            { elem }
+        }
+    }.to_bytes();
+    script.extend_from_slice(program.script.as_bytes());
+
+    let script = Script::from_bytes(script);
+
+    let exec_result = execute_script(script);
+    assert!(exec_result.success);
 }
