8000efebb66587b8a010b65d8360e5abf6600da8
weikengchen <w.k@berkeley.edu>
2024-08-31 21:03:17 +0800

    pass impl tostring

--- src/options.rs
+++ src/options.rs
@@ -68,49 +68,49 @@ impl Options {
         self.map.contains_key(name)
     }
 
-    pub fn get_string(&self, name: &impl ToString) -> Result<&String> {
+    pub fn get_string(&self, name: impl ToString) -> Result<&String> {
         match self.map.get(&name.to_string()) {
             Some(OptionsEntry::String(v)) => Ok(v),
             _ => Err(Error::msg("The corresponding option must be a string"))
         }
     }
 
-    pub fn get_binary(&self, name: &impl ToString) -> Result<&[u8]> {
+    pub fn get_binary(&self, name: impl ToString) -> Result<&[u8]> {
         match self.map.get(&name.to_string()) {
             Some(OptionsEntry::Binary(v)) => Ok(v),
             _ => Err(Error::msg("The corresponding option must be a binary"))
         }
     }
 
-    pub fn get_multi_binary(&self, name: &impl ToString) -> Result<&[Vec<u8>]> {
+    pub fn get_multi_binary(&self, name: impl ToString) -> Result<&[Vec<u8>]> {
         match self.map.get(&name.to_string()) {
             Some(OptionsEntry::MultiBinary(v)) => Ok(v),
             _ => Err(Error::msg("The corresponding option must be a multi binary"))
         }
     }
 
-    pub fn get_u32(&self, name: &impl ToString) -> Result<u32> {
+    pub fn get_u32(&self, name: impl ToString) -> Result<u32> {
         match self.map.get(&name.to_string()) {
             Some(OptionsEntry::U32(v)) => Ok(*v),
             _ => Err(Error::msg("The corresponding option must be a u32"))
         }
     }
 
-    pub fn get_multi_u32(&self, name: &impl ToString) -> Result<&[u32]> {
+    pub fn get_multi_u32(&self, name: impl ToString) -> Result<&[u32]> {
         match self.map.get(&name.to_string()) {
             Some(OptionsEntry::MultiU32(v)) => Ok(v),
             _ => Err(Error::msg("The corresponding option must be a multi u32"))
         }
     }
 
-    pub fn get_u64(&self, name: &impl ToString) -> Result<u64> {
+    pub fn get_u64(&self, name: impl ToString) -> Result<u64> {
         match self.map.get(&name.to_string()) {
             Some(OptionsEntry::U64(v)) => Ok(*v),
             _ => Err(Error::msg("The corresponding option must be a u64"))
         }
     }
 
-    pub fn get_multi_u64(&self, name: &impl ToString) -> Result<&[u64]> {
+    pub fn get_multi_u64(&self, name: impl ToString) -> Result<&[u64]> {
         match self.map.get(&name.to_string()) {
             Some(OptionsEntry::MultiU64(v)) => Ok(v),
             _ => Err(Error::msg("The corresponding option must be a multi u64"))
